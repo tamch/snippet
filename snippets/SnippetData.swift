8dbe71617ba52278729dc2c4bc7b8f7867fc316b
tamch <tam@medias-in-res.com>
2017-04-28 10:35:32 +0800

    Chapter New Snippets

---
+++ snippets/SnippetData.swift
@@ -0,0 +1,19 @@
+//
+//  SnippetData.swift
+//  snippets
+//
+//  Created by tam on 26/4/17.
+//  Copyright © 2017 com.sap.cp. All rights reserved.
+//
+
+import Foundation
+
+struct SnippetData {
+    
+    init() {
+        print ("new snippet created")
+    }
+    
+}
+
+
--- snippets/ViewController.swift
+++ snippets/ViewController.swift
@@ -9,17 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var data: [SnippetData] = [SnippetData]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func createNewSnippet(_ sender: Any) {
+        let newSnippet = SnippetData()
+        data.append(newSnippet)
     }
-
-
 }
 
